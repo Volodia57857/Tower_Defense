ecc66ff6484e478bfd85bd87a8d4b1884739641f
senya2105 <aarseniy2105@gmail.com>
2025-08-19 17:21:37 +0200

    --
    --

---
+++ Assets/EnemySettings/EnemyScripts/SpawnerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyPrefab;  
+    [SerializeField]
+    private Transform spawnPoint;    
+    [SerializeField]
+    private float spawnInterval = 2f; 
+    [SerializeField]
+    private int enemyCount = 5;       
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        for (int i = 0; i < enemyCount; i++)
+        {
+            Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+}
